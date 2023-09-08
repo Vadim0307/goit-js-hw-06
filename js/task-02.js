05a2714028a9a79c16ce3b81b695f117e3eb9ee0
Vadim0307 <133796896+Vadim0307@users.noreply.github.com>
2023-09-08 22:01:22 +0300

    Add 08.09.2023 22:00
    task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const newIngredients = [];
+const ingredientsUl = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  let newIngredient = document.createElement("li");
+  newIngredient.classList.add("item");
+  newIngredient.textContent = ingredient;
+  newIngredients.push(newIngredient);
+  return;
+});
+
+ingredientsUl.append(...newIngredients);
