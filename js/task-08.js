877b06f39c47bb0ae135e02003094f36fb035f3b
Vadim0307 <133796896+Vadim0307@users.noreply.github.com>
2023-09-09 16:46:06 +0300

    Add 09.09.2023 16:45
    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onSubmitClick);
+
+function onSubmitClick(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  const dataForm = {
+    email: email.value,
+    password: password.value,
+  };
+
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені");
+  } else {
+    console.log(dataForm);
+    form.reset();
+  }
+}
 
