4ed3b646613f1208dc1d2693bca0a7fbc7c09396
Vadim0307 <133796896+Vadim0307@users.noreply.github.com>
2023-09-09 21:29:26 +0300

    Add 09.09.2023 21:29
    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+const body = document.querySelector('body');
+
+btn.addEventListener('click', changeColor)
+
+function changeColor(el) {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+}
+
